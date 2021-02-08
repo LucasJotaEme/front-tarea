cde21a8c10fe3169b5d7fbcd8fd2216633a5e0eb
Lucas Maldonado <lucasmaldonado10@hotmail.com>
2021-02-08 14:44:45 -0300

    Nuevo proyecto frontEnd

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
